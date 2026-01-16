daae0a6c6fe6dde7b98220902d2567984389be0f
David Smith <17909720+dsmithcloud@users.noreply.github.com>
2026-01-16 16:16:19 -0600

    Add Bambu P2S FTPS sync script
    initial commit

---
+++ bambu-sync.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+# Bambu P2S FTPS settings
+PRINTER_IP="192.168.1.50"        # TODO: set your printer IP
+ACCESS_CODE="YOUR_ACCESS_CODE"   # TODO: set your printer access code
+REMOTE_DIR="/usb/timelapse"      # TODO: adjust after you inspect via lftp
+LOCAL_DIR="$HOME/bambu-timelapse"
+
+# Create local dir if missing
+mkdir -p "$LOCAL_DIR"
+
+# Run lftp mirror
+lftp -u "bblp,$ACCESS_CODE" -e "
+  set ssl:verify-certificate no;
+  set net:max-retries 2;
+  set net:timeout 20;
+  mirror --only-newer --verbose \"$REMOTE_DIR\" \"$LOCAL_DIR\";
+  bye
+" "ftps://$PRINTER_IP:990"
