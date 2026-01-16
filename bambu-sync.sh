0e7fff40b1c3965dfaa64284db9c502d0df1ae57
David Smith <17909720+dsmithcloud@users.noreply.github.com>
2026-01-16 16:52:36 -0600

    updated to include both ipcam and Timelapse paths

--- bambu-sync.sh
+++ bambu-sync.sh
@@ -3,17 +3,26 @@
 # Bambu P2S FTPS settings
 PRINTER_IP="192.168.1.50"        # TODO: set your printer IP
 ACCESS_CODE="YOUR_ACCESS_CODE"   # TODO: set your printer access code
-REMOTE_DIR="/usb/timelapse"      # TODO: adjust after you inspect via lftp
+REMOTE_DIR="/"                   # TODO: adjust after you inspect via lftp - use \ escape char for spaces in path
 LOCAL_DIR="$HOME/bambu-timelapse"
 
 # Create local dir if missing
 mkdir -p "$LOCAL_DIR"
 
-# Run lftp mirror
-lftp -u "bblp,$ACCESS_CODE" -e "
-  set ssl:verify-certificate no;
-  set net:max-retries 2;
-  set net:timeout 20;
-  mirror --only-newer --verbose \"$REMOTE_DIR\" \"$LOCAL_DIR\";
-  bye
-" "ftps://$PRINTER_IP:990"
+# Run ipcam lftp mirror
+lftp -u bblp,$ACCESS_CODE -e \
+  "set ssl:verify-certificate no; \
+   set net:max-retries 2; \
+   set net:timeout 20; \
+   mirror --only-newer --verbose \"${REMOTE_DIR}/ipcam\" \"${IPCAM_LOCAL_DIR}\"; \
+   bye" \
+  ftps://$PRINTER_IP:990
+
+# Run timelapselftp mirror
+lftp -u bblp,$ACCESS_CODE -e \
+  "set ssl:verify-certificate no; \
+   set net:max-retries 2; \
+   set net:timeout 20; \
+   mirror --only-newer --verbose \"${REMOTE_DIR}/timelapse\" \"${TIMELAPSE_LOCAL_DIR}\"; \
+   bye" \
+  ftps://$PRINTER_IP:990
